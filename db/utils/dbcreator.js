9f400a8bdecd6a948456c57c8c2b713332028e1b
Ashton Hauser <ashton.hauser777@gmail.com>
2018-12-15 02:56:38 +0000

    Passwords are now being hashed succesfully, no login or register button implemented yet.

---
+++ db/utils/dbcreator.js
@@ -0,0 +1,23 @@
+const pg = require('pg');
+
+const client = new pg.Client({
+  user: 'development',
+  host: 'localhost',
+  database: 'BestOf3',
+  password: 'development',
+  port: 5432
+});
+client.connect();
+module.exports = {
+
+  setEmailAndPassword: function(email, password){
+    return client.query("INSERT INTO users (email, password, exp) VALUES ('" + email + "', " + "'" + password + "', 0);"
+  ).catch(e => console.error(e));
+  },
+
+  grabPasswordByEmail: function(email){
+    return client.query("SELECT password FROM users WHERE email= '" + email + "';"
+    ).catch(e => console.error(e));
+  }
+
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const dbUtils = require('../db/utils/dbcreator.js');
+const hashes = {};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+router.get('/register', function(req, res) {
+  res.render('register');
+});
+
+router.post('/register', function(req, res) {
+  const salt = bcrypt.genSaltSync(saltRounds);
+  hashes[req.body.email] = bcrypt.hashSync(req.body.password, salt);
+  dbUtils.setEmailAndPassword(req.body.username, hashes[req.body.email]);
+
+  res.render('index');
+});
+
+router.get('/login', function(req, res) {
+  res.render('login');
+})
+
+router.post('/login', function(req, res) {
+  if (bcrypt.compareSync(req.body.password, hashes[req.body.email])) {
+    res.render('index');
+  }
+})
 
 module.exports = router;
