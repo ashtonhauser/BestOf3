a793fd8380a3346584a171bb6384f0695d4ea48f
Tristan <tristandeering@hotmail.com>
2019-01-05 21:35:42 +0000

    gif links centered

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,15 +1,37 @@
+// $(document).ready(function(){
+//   $('.single').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 3
+//   })
+//   $('.local').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 3
+//   })
+//   $('.online').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 3,
+//     arrows: false
+//   })
+// });
+
 $(document).ready(function(){
   $('.single').slick({
-    slidesToShow: 3,
-    slidesToScroll: 3
   })
   $('.local').slick({
-    slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    infinite: true,
+    cssEase: 'linear',
+    arrows: true
   })
   $('.online').slick({
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    arrows: false
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
   })
 });
