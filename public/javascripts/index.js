c739f2b119bfb2e564f03a31ab7cff90ebfc384b
Tristan <tristandeering@hotmail.com>
2019-01-05 10:19:45 +0000

    landing page adjusted to be game index

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   $('.single').slick({
-    setting-name: setting-value
-  });
+    slidesToShow: 3,
+    slidesToScroll: 3
+  })
+  $('.local').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3
+  })
+  $('.online').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false
+  })
 });
